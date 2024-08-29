519d467c68b803fbf413c0f687b0e8843953f92b
Giuseppe Incurvati <g.incurvati.c@gmail.com>
2024-08-29 16:05:31 +0200

    Add SWR library

--- project_4/src/Root.jsx
+++ project_4/src/Root.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import { Navbar } from "./components/Navbar";
+import { SWRConfig } from "swr";
+
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export function Root() {
   return (
-    <BrowserRouter>
-      <App />
-      <Navbar />
-    </BrowserRouter>
+    <SWRConfig value={{ fetcher }}>
+      <BrowserRouter>
+        <App />
+        <Navbar />
+      </BrowserRouter>
+    </SWRConfig>
   );
 }
