16a6207f66d53401a097807247d2c7ba04c50d63
Giuseppe Incurvati <g.incurvati.c@gmail.com>
2024-09-23 19:23:37 +0200

    User page Css complete

--- project_4/src/components/ProfSearchPage.jsx
+++ project_4/src/components/ProfSearchPage.jsx
@@ -2,19 +2,27 @@ import { DATA } from "../database";
 import customSearch from "../styles/SearchPage.module.css";
 
 export function ProfSearchPage() {
-    const profDb = DATA;
+  const profDb = DATA;
 
-    return (
-      <div className={customSearch.filtered}>
-        <ul className={customSearch.ul}>{profDb.map((card) => (
-            <li key={card.id} className={customSearch.li}>
-                <h2>{card.name}</h2>
-                <img src={card.image}/>
-                <p>{card.description}</p>
-                <p>{card.program}</p>
-            </li>
+  return (
+    <div className={customSearch.filtered}>
+      <ul className={customSearch.ul}>
+        {profDb.map((card) => (
+          <li key={card.id} className={customSearch.li}>
+            <h2>{card.username}</h2>
+            <img src={card.image} />
+            <p>{card.description}</p>
+            <p>
+              {card.program.map((program) => (
+                <div>
+                  <p>{program.name}</p>
+                  <img src={program.icon} />
+                </div>
+              ))}
+            </p>
+          </li>
         ))}
-        </ul>    
-      </div>
-  )
-  }
+      </ul>
+    </div>
+  );
+}
--- project_4/src/pages/GeneralSetting.jsx
+++ project_4/src/pages/GeneralSetting.jsx
@@ -1,19 +1,247 @@
 import { DATA } from "../database";
+import style from "../styles/GeneralSetting.module.css";
+import iconModify from "../assets/icon_modify.svg";
+import { Link, useParams } from "react-router-dom";
+
+import { useState } from "react";
+import { useShowToggle } from "../hooks/useShowToggle";
+import iconClose from "../assets/xmark-solid.svg";
 
 const db = DATA;
 export function GeneralSetting() {
-  const general = db.general;
+  // Da usare nel momento in cui avremo un database
+  //   const { id } = useParams();
+  //   const {data, error, mutate} = useSWR(`linkDatabase/${id}`)
 
+  // Controllo stato per i toggle
+  const [toggleSurname, onToggleSurname] = useShowToggle();
+  const [toggleFirstname, onToggleFirstname] = useShowToggle();
+  const [toggleAnnoNascita, onToggleAnnoNascita] = useShowToggle();
+  const [toggleLuogoNascita, onToggleLuogoNascita] = useShowToggle();
+
+  // Cambio classi
+  const [toggleAsideHamburger, onToggleAsideHamburger] = useShowToggle();
+
+  // Recupero User grazie a ID preso da useParams
+  const id = "5";
+  const user = db.find((user) => user.id.toString() === id);
+
+  // Ottengo Solo i general dello user
+  const general = user.general;
+
+  // Costanti per cambiare la descrizione
+  // const [inputDescription, setInputDescription] = useState("");
+  // const [userDescription, setUserDescription] = useState(user.description);
+
+  // Handle Username
+  // function handleChangeUsername(e) {
+  //   e.preventDefault();
+  //   setUser(inputDescription);
+  // }
+
+  // Handle Image
+  // function handleChangeLinkImage(e) {
+  //   setInputImage(e.target.value);
+  // }
+
+  // function handleChangeImage(e) {
+  //   e.preventDefault();
+  //   setUserImage(inputImage);
+  // }
+
+  // // Handle Description
+  // function handleChangeInputDescription(e) {
+  //   setInputDescription(e.target.value);
+  // }
+
+  // function handleChangeDescription(e) {
+  //   e.preventDefault();
+  //   setUserDescription(inputDescription);
+  // }
   return (
-    <div>
-      <h1>General Setting</h1>
-      <h3>Nome</h3>
-      <p>{general.firstName}</p>
-      <p>{general.surName}</p>
-      <p>Anno di nascita</p>
-      <p>{general.annoNascita}</p>
-      <p>Luogo di Nascita</p>
-      <p>{general.luogoNascita}</p>
+    <div className={style.container}>
+      <div className={style.container_noTitle}>
+        {/* Hamburger menù */}
+        <div
+          className={
+            toggleAsideHamburger ? style.bs_none : style.hamburger_container
+          }
+        >
+          <div
+            onClick={onToggleAsideHamburger}
+            className={toggleAsideHamburger ? style.dn : style.hamburger}
+          >
+            <div className={style.line}></div>
+            <div className={style.line}></div>
+            <div className={style.line}></div>
+          </div>
+          <div
+            className={
+              toggleAsideHamburger ? style.hamburger_content : style.dn
+            }
+          >
+            <div className={style.hamburger_content_top}>
+              <p>SETTINGS</p>
+
+              <img
+                className={style.icon_close}
+                src={iconClose}
+                alt="Close Menù"
+                onClick={onToggleAsideHamburger}
+              />
+            </div>
+            <Link to="/user" className={style.link}>
+              USER SETTING
+            </Link>
+            {/* Presentation sono i dati anagrafaci */}
+            <Link to="/user/presentation_setting" className={style.link}>
+              Experience SETTING
+            </Link>
+            {/* Tutti i progetti caricati e cioè un array dei progetti inseriti, da qui può toglierli e inserirli */}
+            <Link to="/user/project_setting" className={style.link}>
+              PROJECT SETTING
+            </Link>
+          </div>
+        </div>
+
+        {/* Aside tutto schermo laterale dx */}
+        <aside className={style.aside}>
+          <div className={style.aside_sticky}>
+            <Link to="/user" className={style.link}>
+              User Setting
+            </Link>
+            {/* Presentation sono i dati anagrafaci */}
+            <Link to="/user/presentation_setting" className={style.link}>
+              Experience Setting
+            </Link>
+            {/* Tutti i progetti caricati e cioè un array dei progetti inseriti, da qui può toglierli e inserirli */}
+            <Link to="/user/project_setting" className={style.link}>
+              Project Setting
+            </Link>
+          </div>
+        </aside>
+
+        {/* Sezione Centrale */}
+        <div className={style.content}>
+          <h1 className={style.h1}>General Setting of {user.username}!</h1>
+
+          {/* Nome scelto */}
+          <div className={style.container_accept}>
+            <p className={style.p_accept}>First Name: {general.firstName}</p>
+            <img
+              src={iconModify}
+              alt="Modify Icon"
+              className={style.icon_change}
+              name="image"
+              onClick={onToggleFirstname}
+            />
+            {/* Change Firstname */}
+            {toggleFirstname && (
+              <div className={style.container_change}>
+                <input
+                  type="text"
+                  // onChange={handleChangeInputDescription}
+                  placeholder={general.firstName}
+                  className={style.input}
+                ></input>
+                <button
+                  // onClick={handleChangeUsername}
+                  className={style.buttonSave}
+                >
+                  Save
+                </button>
+              </div>
+            )}
+          </div>
+          {/* Cognome Scelto */}
+          <div className={style.container_accept}>
+            <p className={style.p_accept}>Surname: {general.surName}</p>
+            <img
+              src={iconModify}
+              alt="Modify Icon"
+              className={style.icon_change}
+              name="image"
+              onClick={onToggleSurname}
+            />
+
+            {/* Change Surname */}
+            {toggleSurname && (
+              <div className={style.container_change}>
+                <input
+                  type="text"
+                  // onChange={handleChangeInputDescription}
+                  placeholder={general.surName}
+                  className={style.input}
+                ></input>
+                <button
+                  // onClick={handleChangeUsername}
+                  className={style.buttonSave}
+                >
+                  Save
+                </button>
+              </div>
+            )}
+          </div>
+          {/* Anno di Nascita Scelto */}
+          <div className={style.container_accept}>
+            <p className={style.p_accept}>You both in {general.annoNascita}</p>
+            <img
+              src={iconModify}
+              alt="Modify Icon"
+              className={style.icon_change}
+              name="image"
+              onClick={onToggleAnnoNascita}
+            />
+            {/* Change Anno di Nascita */}
+            {toggleAnnoNascita && (
+              <div className={style.container_change}>
+                <input
+                  type="text"
+                  // onChange={handleChangeInputDescription}
+                  placeholder={general.annoNascita}
+                  className={style.input}
+                ></input>
+                <button
+                  // onClick={handleChangeUsername}
+                  className={style.buttonSave}
+                >
+                  Save
+                </button>
+              </div>
+            )}
+          </div>
+          {/* Luogo di Nascita Scelto */}
+          <div className={style.container_accept}>
+            <p className={style.p_accept}>
+              You are from {general.luogoNascita}
+            </p>
+            <img
+              src={iconModify}
+              alt="Modify Icon"
+              className={style.icon_change}
+              name="image"
+              onClick={onToggleLuogoNascita}
+            />
+            {/* Change Luogo di nascita */}
+            {toggleLuogoNascita && (
+              <div className={style.container_change}>
+                <input
+                  type="text"
+                  // onChange={handleChangeInputDescription}
+                  placeholder={general.luogoNascita}
+                  className={style.input}
+                ></input>
+                <button
+                  // onClick={handleChangeUsername}
+                  className={style.buttonSave}
+                >
+                  Save
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
