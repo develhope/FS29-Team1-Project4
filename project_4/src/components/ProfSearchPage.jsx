e284047433ed08826bdb38d1b2cf60ef48968513
Fabrizio <faber96@gmail.com>
2024-09-24 19:18:13 +0200

    cards restyle

--- project_4/src/components/ProfSearchPage.jsx
+++ project_4/src/components/ProfSearchPage.jsx
@@ -8,10 +8,16 @@ export function ProfSearchPage() {
       <div className={customSearch.filtered}>
         <ul className={customSearch.ul}>{profDb.map((card) => (
             <li key={card.id} className={customSearch.li}>
-                <h2>{card.name}</h2>
+                <h2>{card.username}</h2>
                 <img src={card.image}/>
-                <p>{card.description}</p>
-                <p>{card.program}</p>
+                <p>{card.description}</p> 
+                <div className={customSearch.program}>{card.program.map((programs) => (
+                    <div key={programs.id} className={customSearch.program_content}>
+                        <img src={programs.icon} />
+                        <p>{programs.name}</p>
+                    </div>
+))}
+</div>
             </li>
         ))}
         </ul>    
