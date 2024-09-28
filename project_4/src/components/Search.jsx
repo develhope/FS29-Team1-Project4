2395c1fc5c3c5220d3dc025a4a5284f562968224
Giuseppe Incurvati <g.incurvati.c@gmail.com>
2024-09-28 13:37:12 +0200

    Added css for search input in navbar

--- project_4/src/components/Search.jsx
+++ project_4/src/components/Search.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import nav_container from "../styles/Navbar.module.css";
+import { DATA } from "../database";
+
+const users = DATA;
 
 export function Search() {
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
 
+  // Filter users
+  const usersFiltered = users.filter((user) =>
+    user.username.toUpperCase().includes(search.toUpperCase())
+  );
+
+  // Navigate whit name
   function handlePlaySearch(e) {
     navigate("/search");
   }
+
   // cambiare il navigate, e capire come.
   function handleChoice(e) {
-    // chiedere se va inserito
-    e.stopPropagation();
     navigate(`/${e.target.value}`);
   }
   return (
@@ -32,6 +40,15 @@ export function Search() {
       <button type="button" onClick={handlePlaySearch}>
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
+      {search !== "" && (
+        <div className={nav_container.drop_search}>
+          {usersFiltered.map((user) => (
+            <p key={user.id} className={nav_container.drop_search_li}>
+              {user.username}
+            </p>
+          ))}
+        </div>
+      )}
     </form>
   );
 }
